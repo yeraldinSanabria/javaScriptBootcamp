81292ae91e0afdb02cdda8d07060fc85ccdf8ff0
yeraldin-sanabria <yeraldin.sanabria@misena.edu.co>
2024-09-21 23:52:02 -0500

    do while, ejercicios

--- dowhile.js
+++ dowhile.js
@@ -1,65 +1,98 @@
 // do while 
+// calcular el area de un circulo,cuadrado, triangulo 
+// area del circulo a= pi* (r*r)
+// area del cuadrado a= lado *lado
+// area de un trinagulo a= base*altura /2
 
-
-let k,f,c;
-
+let areaCirculo, areaCudrado, areaTriangulo;
+let radio, lado, base, altura;
+const pi = 3.1416;
 let opcion;
 
-opcion = parseInt(prompt(`1. kelvin a celsius \n 2. De kelvin a Fahrenheit \n 3. Fahrenheit a celius  \n 4. Salir`));
 
-while(opcion != 4){
+do {
+  opcion = parseInt(prompt(`1. Area del circulo\n 2. Area del cuadrado\n 3. Area del tringulo \n 4. Salir`))
 
-   
-
-  switch(opcion){
-
-
-    case 1: alert("Area del circulo");
-    // A = pi * r*r
-    r = parseFloat(prompt("Ingrese el valor del radio"));
-
-    ac = pi * (r*r);
-
-    alert(`El area del circulo es: ${ac}`);
-  
-    break;
-case 2: alert("Area del cuadrado");
-
-   // A = lado * lado
-
-   lado = parseFloat(prompt("Ingrese el valor del lado"));
-
-   acu = (lado*lado);
-
-   alert(`El area del cuadrado es: ${acu}`);
-
-    break;
-
-    case 3: alert("Area del triangulo");
-
-    // A = B*H/2
-
-    b = parseFloat(prompt("Ingrese el valor de la base"));
-    h = parseFloat(prompt("Ingrese el valor de la altura"));
-
-    at = (b*h)/2;
-
-    alert(`El area del triangulo es: ${at}`);
-
-    break;
-case 4:
-      alert("SALIENDO.....");
-      //estado = false;
-
-    break;
-
-    default: 
-    alert("Opcion no valida");
-
-  
-
+  switch (opcion) {
+    case 1:
+      alert("Area del circulo");
+      radio = (parseFloat(prompt(`Ingrese el valor del radio`)));
+      areaCirculo = pi * (radio * radio);
+      alert(`El area del circulo es: ${areaCirculo}`);
+      break;
+    case 2:
+      alert("Area del cuadrado");
+      lado = (parseFloat(prompt(`Ingrese el valor del lad`)));
+      areaCudrado = (lado * lado);
+      alert(`El area del cuadrado es: ${areaCudrado}`);
+      break;
+    case 3:
+      alert("Area del triangulo");
+      base = (parseFloat(prompt(`Ingrese el valor de la base`)));
+      altura = (parseFloat(prompt(`Ingrese el valor de la altura`)));
+      areaTriangulo = (base * altura) / 2;
+      alert(`El area del triangulo es: ${areaTriangulo}`)
+      break;
+    case 4:
+      alert("saliendo...")
+      break;
+    default:
+      alert("Opcion no valida")
   }
+} while (opcion != 4);
 
-  opcion = parseInt(prompt(`1. Area Circulo \n 2. Area del Cuadrado \n 3. Area del Triangulo \n 4. Salir`));
+// converion de temperaturas
 
-}
+let kac, kaf, fac, fak, cak, caf;
+let kac1, kaf1, fac1, fak1, cak1, caf1;
+let conversion;
+let opcion1;
+let estado=true;
+
+do {
+  opcion1 = parseInt(prompt(`1.Kelvin a Celsius \n 2. Kelvin a Fahrenheit\n 3.Fahrenheit a Celsius \n 4.Fahrenheit a Kelvin \n 5.Celsius a Kelvin \n 6.Celsius a Fahrenheit\n 7. Salir`))
+
+  switch (opcion1) {
+    case 1:
+      alert("Conversion de Kelvin a Celsius");
+      kac1 = (parseFloat(prompt(`Ingrese la temperatura en  kelvin`)));
+      kac = (kac1-273.15).toFixed(3);
+      alert(`Equivale a: ${kac} Celsius`);
+      break;
+    case 2:
+      alert("Conversion de Kelvin a Fahrenheit");
+      kaf1 = (parseFloat(prompt(`Ingrese la temperatura en  kelvin`)));
+      kaf = (((kaf1-273.15)*9/5)+32).toFixed(3);
+      alert(`Equivale a: ${kaf} Fahrenheit`);
+      break;
+    case 3:
+      alert("Conversion de Fahrenheit a Celsius");
+      fac1 = (parseFloat(prompt(`Ingrese la temperatura en Fahrenheit`)));
+      fac = ((fac1-32)*5/9).toFixed(3);
+      alert(`Equivale a: ${fac} Celsius`)
+      break;
+    case 4:
+      alert("Conversion de Fahrenheit a Kelvin");
+      fak1 = (parseFloat(prompt(`Ingrese la temperatura en Fahrenheit`)));
+      fak = (((fak1-32)*5/9)+273.15).toFixed(3);
+      alert(`Equivale a: ${fak} Kelvin`)
+      break;
+    case 5:
+      alert("Conversion de Celsius a Kelvin");
+      cak1 = (parseFloat(prompt(`Ingrese la temperatura en Celsius`)));
+      cak = (cak1+273.15).toFixed(3);
+      alert(`Equivale a: ${cak} Kelvin`)
+      break;
+    case 6:
+      alert("Conversion de Celsius a Fahrenheit");
+      caf1 = (parseFloat(prompt(`Ingrese la temperatura en Celsius`)));
+      caf = (((caf1*9)/5)+32).toFixed(3) ;
+      alert(`Equivale a: ${caf} Fahrenheit`)
+      break;
+    case 7:
+      alert("saliendo...")
+      break;
+    default:
+      alert("Opcion no valida")
+  }
+} while (opcion1 !=7);
--- while.js
+++ while.js
@@ -1,79 +1,121 @@
-// let numero = 1;
+let numero = 1;
 
-// while (numero <= 10) {
-//   console.log(numero)
-//   numero++;
-// }
+while (numero <= 10) {
+  console.log(numero)
+  numero++;
+}
 
-// // sumar numeros positivos
-
-
-// let num2;
-// let sum2 = 0;
-
-// while ((num2 = parseInt(prompt("ingrese un numero positivo "))) > 0) {
-
-//   sum2 += num2;
-//   console.log(sum2);
-
-// }
-// console.log("la suma total es :", sum2)
+// sumar numeros positivos
 
 
-// // menu de opciones 
+let num2;
+let sum2 = 0;
 
-// let opcion;
-// let estado = true;
-// let raiz, seno, coseno, numero2;
+while ((num2 = parseInt(prompt("ingrese un numero positivo "))) > 0) {
 
-// // operaciones a relaizar 
+  sum2 += num2;
+  console.log(sum2);
 
-// // 1. raiz 
-// // 2. seno
-// // 3. coseno
+}
+console.log("la suma total es :", sum2)
 
-// while (estado) {
-//   opcion = (parseInt(prompt(`seleciones una opcion \n 1 .Raiz \n 2 . Seno \n 3 . Coseno \n 4 . Salir `)));
 
-//   switch (opcion) {
-//     case 1:
-//       alert("selecciono Raiz")
-//       numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la raiz`)))
-//       raiz = Math.sqrt(numero2);
-//       alert(`La raiz de ${numero2} Es: ${raiz}`)
-//       break;
-//     case 2:
-//       alert("selecciono Seno")
-//       numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la seno`)))
-//       seno = Math.sin(numero2);
-//       alert(`El seno  de ${numero2} Es: ${seno}`)
-//       break;
-//     case 3:
-//       alert("selecciono Coseno")
-//       numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la coseno`)))
-//       coseno = Math.cos(numero2);
-//       alert(`El coseno  de ${numero2} Es: ${coseno}`)
-//       break;
-//     case 4:
-//       alert("saliendo...")
-//       estado = false;
+// menu de opciones 
 
-//     default:
-//       alert("La opcion no existe")
+let opcion;
+let estado = true;
+let raiz, seno, coseno, numero2;
 
-//       break;
-//   }
+// operaciones a relaizar 
 
-// }
+// 1. raiz 
+// 2. seno
+// 3. coseno
+
+while (estado) {
+  opcion = (parseInt(prompt(`seleciones una opcion \n 1 .Raiz \n 2 . Seno \n 3 . Coseno \n 4 . Salir `)));
+
+  switch (opcion) {
+    case 1:
+      alert("selecciono Raiz");
+      numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la raiz`)));
+      raiz = Math.sqrt(numero2);
+      alert(`La raiz de ${numero2} Es: ${raiz}`);
+      break;
+    case 2:
+      alert("selecciono Seno");
+      numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la seno`)));
+      seno = Math.sin(numero2);
+      alert(`El seno  de ${numero2} Es: ${seno}`);
+      break;
+    case 3:
+      alert("selecciono Coseno");
+      numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la coseno`)));
+      coseno = Math.cos(numero2);
+      alert(`El coseno  de ${numero2} Es: ${coseno}`);
+      break;
+    case 4:
+      alert("saliendo...")
+      estado = false;
+
+    default:
+      alert("La opcion no existe")
+
+      break;
+  }
+
+  // calcular el area de un circulo,cuadrado, triangulo 
+// area del circulo a= pi* (r*r)
+// area del cuadrado a= lado *lado
+// area de un trinagulo a= base*altura /2
+
+let areaCirculo, areaCudrado, areaTriangulo;
+let radio, lado, base, altura;
+const pi = 3.1416;
+let opcion;
+
+while(opcion !=4 ){
+  
+  opcion = parseInt(prompt(`1. Area del circulo\n 2. Area del cuadrado\n 3. Area del tringulo \n 4. Salir`))
+
+  switch (opcion) {
+    case 1:
+      alert("Area del circulo");
+      radio = (parseFloat(prompt(`Ingrese el valor del radio`)));
+      areaCirculo = pi * (radio * radio);
+      alert(`El area del circulo es: ${areaCirculo}`);
+      break;
+    case 2:
+      alert("Area del cuadrado");
+      lado = (parseFloat(prompt(`Ingrese el valor del lad`)));
+      areaCudrado = (lado * lado);
+      alert(`El area del cuadrado es: ${areaCudrado}`);
+      break;
+    case 3:
+      alert("Area del triangulo");
+      base = (parseFloat(prompt(`Ingrese el valor de la base`)));
+      altura = (parseFloat(prompt(`Ingrese el valor de la altura`)));
+      areaTriangulo = (base * altura) / 2;
+      alert(`El area del triangulo es: ${areaTriangulo}`)
+      break;
+    case 4:
+      alert("saliendo...")
+      break;
+    default:
+      alert("Opcion no valida") 
+  }
+}
+
+}
 
   // juego de adicinar el numero 
 
   let aleatorio = Math.floor(Math.random() * 50) + 1;
   let numero3 = true;
-
+  
   while (numero3) {
 
-    let numeroaadivinar = parseInt(prompt("ingresa un numero del 1 al 50"))
+    let numeroaadivinar = parseInt(prompt("ingresa un numero del 1 al 50"));
     console.log(aleatorio)
 
     if (aleatorio === numeroaadivinar) {
