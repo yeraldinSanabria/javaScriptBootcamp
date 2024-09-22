caa48df3f9076636dd2880b59da34c113c070225
yeraldin-sanabria <yeraldin.sanabria@misena.edu.co>
2024-09-22 18:49:01 -0500

    ejercico de * y constraseña

--- cicloFor.js
+++ cicloFor.js
@@ -1,54 +1,62 @@
-// ciclo for 
+// // ciclo for 
 
-// incremento 
+// // incremento 
 
-for (let i = 1; i <= 4; i++) {
-    console.log(i)
+// for (let i = 1; i <= 4; i++) {
+//     console.log(i)
 
+// }
+
+// // decremento 
+// for (let j = 20; j >= 1; j -= 1) { //puedo colocar un +=3 eso quiere decir que va a incrementar ede 3 en 3 
+//     console.log(j)
+
+// }
+
+// // tablas de multiplicar 
+
+// // let tabla = parseInt(prompt(("ingrese el número de la tabla que desea observar")));
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(` ${4} * ${i} = ${4 * i}`)
+// }
+
+// // suma de numeros positivos donde el usuario colcoa el numero hasta donde llevara el for 
+
+// // let n = parseInt(prompt("Introduce un nuemro positivo"));
+// let suma = 0;
+
+// for (let i = 1; i <= 5; i++) {
+//     suma += 5;
+//     console.log(`la suma de los numeros del 1 al ${5} es : ${suma}`);
+// }
+
+// // todas las tablas
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`La tabla del ${i}`)
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i * j}`);
+//     }
+//     console.log(" ")
+// }
+
+// // suma de los 100 primeros numeros impares
+
+// let sum = 0;
+// let contador = 0;
+
+// for (let i = 1; contador <100; i += 2) {
+//     sum += i;
+//     contador++;
+// }
+// console.log(`la suma de los primeros 100 números impares es de ${sum}`)
+
+// ejercicios de los * 
+
+let ingreseNumero = parseInt(prompt("introduce un numero"));
+
+for (let i = 1; i <= ingreseNumero; i++) {
+    console.log("*".repeat(i));
 }
-
-// decremento 
-for (let j = 20; j >= 1; j -= 1) { //puedo colocar un +=3 eso quiere decir que va a incrementar ede 3 en 3 
-    console.log(j)
-
-}
-
-// tablas de multiplicar 
-
-// let tabla = parseInt(prompt(("ingrese el número de la tabla que desea observar")));
-
-for (let i = 1; i <= 10; i++) {
-    console.log(` ${4} * ${i} = ${4 * i}`)
-}
-
-// suma de numeros positivos donde el usuario colcoa el numero hasta donde llevara el for 
-
-// let n = parseInt(prompt("Introduce un nuemro positivo"));
-let suma = 0;
-
-for (let i = 1; i <= 5; i++) {
-    suma += 5;
-    console.log(`la suma de los numeros del 1 al ${5} es : ${suma}`);
-}
-
-// todas las tablas
-
-
-for (let i = 1; i <= 10; i++) {
-    console.log(`La tabla del ${i}`)
-    for (let j = 0; j <= 10; j++) {
-        console.log(`${i} * ${j} = ${i * j}`);
-    }
-    console.log(" ")
-}
-
-// suma de los 100 primeros numeros impares
-
-let sum = 0;
-let contador = 0;
-
-for (let i = 1; contador <100; i += 2) {
-    sum += i;
-    contador++;
-}
-console.log(`la suma de los primeros 100 números impares es de ${sum}`)
--- while.js
+++ while.js
@@ -1,130 +1,156 @@
-let numero = 1;
+// let numero = 1;
 
-while (numero <= 10) {
-  console.log(numero)
-  numero++;
-}
+// while (numero <= 10) {
+//   console.log(numero)
+//   numero++;
+// }
 
-// sumar numeros positivos
-
-
-let num2;
-let sum2 = 0;
-
-while ((num2 = parseInt(prompt("ingrese un numero positivo "))) > 0) {
-
-  sum2 += num2;
-  console.log(sum2);
-
-}
-console.log("la suma total es :", sum2)
+// // sumar numeros positivos
 
 
-// menu de opciones 
+// let num2;
+// let sum2 = 0;
 
-let opcion;
-let estado = true;
-let raiz, seno, coseno, numero2;
+// while ((num2 = parseInt(prompt("ingrese un numero positivo "))) > 0) {
 
-// operaciones a relaizar 
+//   sum2 += num2;
+//   console.log(sum2);
 
-// 1. raiz 
-// 2. seno
-// 3. coseno
+// }
+// console.log("la suma total es :", sum2)
 
-while (estado) {
-  opcion = (parseInt(prompt(`seleciones una opcion \n 1 .Raiz \n 2 . Seno \n 3 . Coseno \n 4 . Salir `)));
 
-  switch (opcion) {
-    case 1:
-      alert("selecciono Raiz");
-      numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la raiz`)));
-      raiz = Math.sqrt(numero2);
-      alert(`La raiz de ${numero2} Es: ${raiz}`);
-      break;
-    case 2:
-      alert("selecciono Seno");
-      numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la seno`)));
-      seno = Math.sin(numero2);
-      alert(`El seno  de ${numero2} Es: ${seno}`);
-      break;
-    case 3:
-      alert("selecciono Coseno");
-      numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la coseno`)));
-      coseno = Math.cos(numero2);
-      alert(`El coseno  de ${numero2} Es: ${coseno}`);
-      break;
-    case 4:
-      alert("saliendo...")
-      estado = false;
+// // menu de opciones 
 
-    default:
-      alert("La opcion no existe")
+// let opcion;
+// let estado = true;
+// let raiz, seno, coseno, numero2;
 
-      break;
-  }
+// // operaciones a relaizar 
 
-  // calcular el area de un circulo,cuadrado, triangulo 
-// area del circulo a= pi* (r*r)
-// area del cuadrado a= lado *lado
-// area de un trinagulo a= base*altura /2
+// // 1. raiz 
+// // 2. seno
+// // 3. coseno
 
-let areaCirculo, areaCudrado, areaTriangulo;
-let radio, lado, base, altura;
-const pi = 3.1416;
-let opcion;
+// while (estado) {
+//   opcion = (parseInt(prompt(`seleciones una opcion \n 1 .Raiz \n 2 . Seno \n 3 . Coseno \n 4 . Salir `)));
 
-while(opcion !=4 ){
-  
-  opcion = parseInt(prompt(`1. Area del circulo\n 2. Area del cuadrado\n 3. Area del tringulo \n 4. Salir`))
+//   switch (opcion) {
+//     case 1:
+//       alert("selecciono Raiz");
+//       numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la raiz`)));
+//       raiz = Math.sqrt(numero2);
+//       alert(`La raiz de ${numero2} Es: ${raiz}`);
+//       break;
+//     case 2:
+//       alert("selecciono Seno");
+//       numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la seno`)));
+//       seno = Math.sin(numero2);
+//       alert(`El seno  de ${numero2} Es: ${seno}`);
+//       break;
+//     case 3:
+//       alert("selecciono Coseno");
+//       numero2 = (parseFloat(prompt(`Ingrese el numero para calcular la coseno`)));
+//       coseno = Math.cos(numero2);
+//       alert(`El coseno  de ${numero2} Es: ${coseno}`);
+//       break;
+//     case 4:
+//       alert("saliendo...")
+//       estado = false;
 
-  switch (opcion) {
-    case 1:
-      alert("Area del circulo");
-      radio = (parseFloat(prompt(`Ingrese el valor del radio`)));
-      areaCirculo = pi * (radio * radio);
-      alert(`El area del circulo es: ${areaCirculo}`);
-      break;
-    case 2:
-      alert("Area del cuadrado");
-      lado = (parseFloat(prompt(`Ingrese el valor del lad`)));
-      areaCudrado = (lado * lado);
-      alert(`El area del cuadrado es: ${areaCudrado}`);
-      break;
-    case 3:
-      alert("Area del triangulo");
-      base = (parseFloat(prompt(`Ingrese el valor de la base`)));
-      altura = (parseFloat(prompt(`Ingrese el valor de la altura`)));
-      areaTriangulo = (base * altura) / 2;
-      alert(`El area del triangulo es: ${areaTriangulo}`)
-      break;
-    case 4:
-      alert("saliendo...")
-      break;
-    default:
-      alert("Opcion no valida") 
-  }
-}
+//     default:
+//       alert("La opcion no existe")
 
-}
+//       break;
+//   }
 
-  // juego de adicinar el numero 
+//   // calcular el area de un circulo,cuadrado, triangulo 
+//   // area del circulo a= pi* (r*r)
+//   // area del cuadrado a= lado *lado
+//   // area de un trinagulo a= base*altura /2
 
-  let aleatorio = Math.floor(Math.random() * 50) + 1;
-  let numero3 = true;
-  
-  while (numero3) {
+//   let areaCirculo, areaCudrado, areaTriangulo;
+//   let radio, lado, base, altura;
+//   const pi = 3.1416;
+//   let opcion;
 
-    let numeroaadivinar = parseInt(prompt("ingresa un numero del 1 al 50"));
-    console.log(aleatorio)
+//   while (opcion != 4) {
 
-    if (aleatorio === numeroaadivinar) {
-      alert("Adivinaste")
-      numero3 = false;
-    } else if (aleatorio >= numeroaadivinar) {
-      alert("El numero es mayor")
-    } else if (aleatorio <= numeroaadivinar) {
-      alert("El numero es menor")
+//     opcion = parseInt(prompt(`1. Area del circulo\n 2. Area del cuadrado\n 3. Area del tringulo \n 4. Salir`))
+
+//     switch (opcion) {
+//       case 1:
+//         alert("Area del circulo");
+//         radio = (parseFloat(prompt(`Ingrese el valor del radio`)));
+//         areaCirculo = pi * (radio * radio);
+//         alert(`El area del circulo es: ${areaCirculo}`);
+//         break;
+//       case 2:
+//         alert("Area del cuadrado");
+//         lado = (parseFloat(prompt(`Ingrese el valor del lad`)));
+//         areaCudrado = (lado * lado);
+//         alert(`El area del cuadrado es: ${areaCudrado}`);
+//         break;
+//       case 3:
+//         alert("Area del triangulo");
+//         base = (parseFloat(prompt(`Ingrese el valor de la base`)));
+//         altura = (parseFloat(prompt(`Ingrese el valor de la altura`)));
+//         areaTriangulo = (base * altura) / 2;
+//         alert(`El area del triangulo es: ${areaTriangulo}`)
+//         break;
+//       case 4:
+//         alert("saliendo...")
+//         break;
+//       default:
+//         alert("Opcion no valida")
+//     }
+//   }
+
+// }
+
+// // juego de adicinar el numero 
+
+// let aleatorio = Math.floor(Math.random() * 50) + 1;
+// let numero3 = true;
+// let numeroaadivinar;
+
+// while (numero3) {
+
+//   numeroaadivinar = parseInt(prompt("ingresa un numero del 1 al 50"));
+//   console.log(aleatorio)
+
+//   if (aleatorio === numeroaadivinar) {
+//     alert("Adivinaste")
+//     numero3 = false;
+//   } else if (aleatorio >= numeroaadivinar) {
+//     alert("El numero es mayor")
+//   } else if (aleatorio <= numeroaadivinar) {
+//     alert("El numero es menor")
+//   }
+
+// }
+
+// ejercicio de contraseña en scrip 
+
+let acceso;
+let contrasena = "HolaMundo.";
+let inicio = true;
+let contador = 0;
+let numeroDeIntentos = 3;
+
+while (inicio && contador < numeroDeIntentos) {
+  acceso = prompt("Ingrese la contaseña para acceder");
+  if (contrasena === acceso) {
+    alert("Acceso Consedido");
+    inicio = false;
+    contador = 3;
+  }else {
+    contador++;
+    alert(`Acceso Denegado \n Llevas  ${contador} intento de ${numeroDeIntentos}`);
+    if (contador === 3) {
+      alert("Intruso Intentando Acceder");
     }
-
   }
+}
+
+
